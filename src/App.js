9114e7338cd0e1c8fd55cd2ba9fda248433dd787
Bryton Haldey <bryton.hadley@gmail.com>
2022-10-12 11:17:52 -0600

    tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Square from "./Square";
+
+import {useState} from 'react'
 
 function App() {
+const [squares, setSquares] = useState(["", "", "", "", "", "", "" , "", ""]);
+const [player, setPlayer] = useState(true);
+
+// const propVariable = " this is my variable choice "
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Square 
+      // propVar = {propVariable}
+      squares = {squares}
+      setSquares = {setSquares}
+      player = {player}
+      setPlayer = {setPlayer}
+      />
     </div>
+
   );
 }
 
