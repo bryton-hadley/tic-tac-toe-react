dcfdf52057b720d0cfe465551b56af7586378422
Bryton Haldey <bryton.hadley@gmail.com>
2022-10-13 15:25:12 -0600

    setting up the click handler

--- src/App.js
+++ src/App.js
@@ -7,16 +7,33 @@ function App() {
 const [squares, setSquares] = useState(["", "", "", "", "", "", "" , "", ""]);
 const [player, setPlayer] = useState(true);
 
+// creating the functio for handleClick and setitng it to an array with 9 empty strings 
+const handleClick = () => {
+  setSquares(["", "", "", "", "", "", "" , "", ""])
+  setPlayer(true)
+}
+
 // const propVariable = " this is my variable choice "
+// creating a new div with the class of container 
+
   return (
     <div className="App">
-      <Square 
-      // propVar = {propVariable}
-      squares = {squares}
-      setSquares = {setSquares}
-      player = {player}
-      setPlayer = {setPlayer}
-      />
+      <div className='container'>
+          {squares.map((value, index) => {
+            
+            return (
+              <Square 
+                setSquares = {setSquares}
+                index = {index}
+                squareValue = {value}
+                squares = {squares}
+                player = {player}
+                setPlayer = {setPlayer}
+              />
+            );
+          })}
+      </div>
+      <button onClick={handleClick}>Reset</button>
     </div>
 
   );
